abb31c16943c3363010b9ad323b3c92c6f51457f
MOHAMMAD-ALI <bacha@tuta.io>
2018-04-09 23:08:03 +0200

    add sum method

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,6 +6,11 @@ def subtract(x, y)
   subtracts = x - y
 end
 
-def sum(x, y)
-
+#def sum([x,y,z])
+#  array.each do |i|
+#     = [] + x
+#end
+def sum(a)
+ sum = 0
+ a.inject(0) {|sum,x| sum + x.to_i}
 end
